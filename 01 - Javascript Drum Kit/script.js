41a6bfd7688eff9496db91cd7b851e4202141557
ben <baronibenoit00@gmail.com>
2023-08-16 11:12:54 +0200

    Optimises the key value conversion in the event listener.

--- 01 - Javascript Drum Kit/script.js
+++ 01 - Javascript Drum Kit/script.js
@@ -9,41 +9,9 @@ keys.forEach((button) => {
 })
 
 document.addEventListener("keydown",(e)=>{
-    const keyCode = e.key.toLowerCase();
-    let audioKey = undefined;
-    switch (keyCode) {
-        case 'a':
-            audioKey = 65;
-            break;
-        case 'z':
-            audioKey = 83;
-            break;
-        case 'e':
-            audioKey = 68;
-            break;
-        case 'r':
-            audioKey = 70;
-            break;
-        case 't':
-            audioKey = 71;
-            break;
-        case 'y':
-            audioKey = 72;
-            break;
-        case 'u':
-            audioKey = 74;
-            break;
-        case 'i':
-            audioKey = 75;
-            break;
-        case 'o':
-            audioKey = 76;
-            break;
-        default:
-            break;
-    }
-    playSound(audioKey);
-    switchOnLightKey(audioKey);
+    const keyCode = e.key.toUpperCase();
+    playSound(keyCode);
+    switchOnLightKey(keyCode);
 });
 
 function playSound(soundCode){
