92041707173620938a52a06136a15d3c45a8a786
ben <baronibenoit00@gmail.com>
2023-08-21 21:06:59 +0200

    Deals with the fetch data

--- 06 - Type ahead/script.js
+++ 06 - Type ahead/script.js
@@ -1,6 +1,19 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
+let jsonData; // Déclarer une variable pour stocker les données JSON
+
 fetch(endpoint)
-  .then(response => response.json()) // Convertir la réponse en JSON
-  .then(data => console.log(data))    // Utiliser les données
-  .catch(error => console.error('Une erreur s\'est produite', error));
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erreur de réseau');
+    }
+    return response.json(); // Convertir la réponse en JSON
+  })
+  .then(data => {
+    jsonData = data; // Stocker les données dans la variable
+    console.log('Données récupérées:', jsonData); // Utiliser les données
+  })
+  .catch(error => console.error('Une erreur s\'est produite :', error));
+
+
+
